244d96b6416de9dff506c0e7a48978d0cfe67138
xdmy1 <152696259+xdmy1@users.noreply.github.com>
2024-05-19 02:29:01 +0300

    Add files via upload

---
+++ w1.2/main.js
@@ -0,0 +1,18 @@
+function openSidebar() {
+    const sidebar = document.querySelector('.mobile-ul')
+    sidebar.style.display = 'flex'
+    const sidebar1 = document.querySelector('.burger')
+    sidebar1.style.display = 'none'
+    const sidebar2 = document.querySelector('.close')
+    sidebar2.style.display = 'flex'
+}
+
+function closeSidebar() {
+    const sidebar3 = document.querySelector('.mobile-ul')
+    sidebar3.style.display = 'none'
+    const sidebar4 = document.querySelector('.burger')
+    sidebar4.style.display = 'flex'
+    const sidebar5 = document.querySelector('.close')
+    sidebar5.style.display = 'none'
+
+}
